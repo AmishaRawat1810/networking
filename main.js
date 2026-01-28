b9be3dc7a7a539777b9aede735119fd2e43f8042
Amisha Rawat <amisharawat@Amishas-MacBook-Pro.local>
2026-01-28 13:01:08 +0530

    feat: creates connection and listens to the response
    * reads the response and sends it to the source

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
-export function add(a: number, b: number): number {
-  return a + b;
+let count = 0;
+const listener = Deno.listen({
+  "port": 8000,
+  "transport": "tcp",
+});
+
+const writer = Deno.stdout.writable.getWriter();
+const decoder = new TextDecoder();
+
+for await (const conn of listener) {
+  console.clear();
+  while (true) {
+    //
+    ++count;
+
+    //Read from the connection and show it in the source's terminal
+    const buf = new Uint8Array(1024);
+    const bytesRead = await conn.read(buf);
+    const response = buf.slice(0, bytesRead);
+    const decodedResponse = decoder.decode(response);
+
+    if (decodedResponse.trim() === "exit") {
+      conn.writable.getWriter().write("See you again...");
+      break;
+    }
+    writer.write(response);
+  }
+  conn.close();
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+console.log("THE END");
